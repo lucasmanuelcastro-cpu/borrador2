7d10d4e2b6bf51f569dbd982bab28cd2d330276e
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-05-02 10:53:09 -0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // --- LÓGICA DE ESTADO Y SINCRONIZACIÓN EL PROFETA ---
 
-const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbz_FVSfmBUPp7AI4icpVT4GjaA9n5e2amaNdMy4c55VgteVv1miJhTJ27akT3Tl1jFGCw/exec";
+const URL_SCRIPT = "https://script.google.com/macros/s/AKfycbyXNXCkF8LSv9DyI0frTk0bnqI6v6hq2qJl8DM09KU4bkCOiUg-FJguo431Hs0wAY6S_w/exec";
 
 let clientesHistoricos = [];
 let ventasPendientes = [];
